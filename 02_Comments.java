35c3c5eaf68b6ce8be77a04df51042b84e90529d
Nainsi yadav <yadavnainsi734@gmail.com>
2026-06-13 11:30:04 +0530

    Update comment formatting in 02_Comments.java

---
+++ 02_Comments.java
@@ -0,0 +1,13 @@
+ --------------COMMENTS--------------
+1. Single-line comment:
+- Use : //
+-example : 
+// this is single line comment 
+
+2. Multi-line comment:
+-Use : /* */
+-Example  : 
+/* this 
+is multi-line
+comment */
+
