72dd9d793db93345ef2a1fa1f9762ba41a16a5a6
Nainsi yadav <yadavnainsi734@gmail.com>
2026-06-15 10:22:45 +0530

    add new java  program file

---
+++ operators.java
@@ -0,0 +1,31 @@
+
+// 1. Arithmetic operator(binary operator/unary operator)
+// Binary operator(+ ,-,*,/,%)
+public class operators {
+    public static void main(String[] args) {
+       int a = 3;
+       int b = 8;
+       System.out.println(a+b); 
+       System.out.println(a-b); 
+       System.out.println(a*b); 
+       System.out.println(a/b); 
+       System.out.println(a%b); 
+          
+    }
+  
+}
+
+
+
+    // Unary operator
+    public static void main(String[] args) {
+        int a = 7;
+        // Increment ------->
+        System.out.println(a++);  //post increment
+        System.out.println(++a);  // pre increment
+        
+        //  Decrement
+        System.out.println(a--);  //post decrement
+        System.out.println(--a);  // pre decrement
+    }
+
