6e46d7095fcc5687c4c62c0b757039797d658dab
Nainsi yadav <yadavnainsi734@gmail.com>
2026-06-25 20:10:19 +0530

    adding package file in java programming

---
+++ packages.java
@@ -0,0 +1,20 @@
+// Package ---->
+/*A Java package is a collection of similer type of  
+sub-packages,interaces,and classes.
+
+-----Built-in Packages----
+They are available in java, including util, lang, awt etc.
+We can import all members of a package using name.* statement
+
+
+----User-defined packages----
+User-defined pachages are those that the users difine. Inside a package,
+ you can have Java files like classes,interfaces,and a package as well(called a sub-package).
+ */
+
+public class packages {
+    public static void main(String[] args) {
+        
+    }
+    
+}
