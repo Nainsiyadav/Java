94ce948e40933cf02786f6a4e1a773dc1670ebf9
Nainsi yadav <yadavnainsi734@gmail.com>
2026-06-26 10:44:27 +0530

    adding examples in java program file

---
+++ packages.java
@@ -0,0 +1,30 @@
+// Package ---->
+/*A Java package is a collection of similer type of  
+sub-packages,interaces,and classes.
+
+-----Built-in Packages----
+They are available in java, including util, lang, awt etc.
+We can import all members of a package using name.* statement
+
+
+----User-defined packages----
+User-defined pachages are those that the users difine. Inside a package,
+ you can have Java files like classes,interfaces,and a package as well(called a sub-package).
+ */
+
+public class packages {
+    public static void main(String[] args) {
+
+        System.out.println("Absolute Value: " + Math.abs(-15));
+        System.out.println("Maximum: " + Math.max(12, 25));
+        System.out.println("Minimum: " + Math.min(12, 25));
+        System.out.println("Square Root: " + Math.sqrt(49));
+        System.out.println("Power: " + Math.pow(3, 4));
+        System.out.println("Round: " + Math.round(5.6));
+        System.out.println("Ceil: " + Math.ceil(5.2));
+        System.out.println("Floor: " + Math.floor(5.8));
+        System.out.println("Cube Root: " + Math.cbrt(64));
+        System.out.println("Random Number: " + Math.random());
+
+    }
+}
