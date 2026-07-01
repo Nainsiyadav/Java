c4e9022f6ae6be6ea9940393f5db027184947b77
Nainsi yadav <yadavnainsi734@gmail.com>
2026-07-01 19:55:12 +0530

    adding while loop

--- loops.java
+++ loops.java
@@ -13,7 +13,7 @@ import javax.security.sasl.SaslException;
 public class loops {
 
     public static void main(String[] args) {
-        /*for(int i =1;i<=5;i++){
+        for(int i =1;i<=5;i++){
             System.out.println(i);
     }
         // In loops, initialization, condition, and change all are optional. any or all of 
@@ -33,17 +33,17 @@ public class loops {
         // reverse loop
         for (int i = n; i >=1; i--) {
           System.out.println(i) ; 
-        }*/
+        }
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter first number : ");
         int a = sc.nextInt();
        
-        /*int sum = 0;
+        int sum = 0;
         for (int i = 1; i < a; i++) {
             sum += i;
         }
-        System.out.println(sum);*/
+        System.out.println(sum);
         int fact = 1;
         for (int i = 2; i <= a; i++) {
             fact *= i;
@@ -51,3 +51,53 @@ public class loops {
         System.out.print("factorial is "+fact);
         }
     }
+
+
+
+// While loop-------
+/*The while loop is used when the number of iterations is not known but the
+terminating condition is known.
+Loop is executed until the given condition evaluates to false. */
+
+import java.util.Scanner;
+
+public class loops {
+
+    public static void main(String[] args) {
+        int i=0;
+        while(i<5){
+            System.out.println(i);
+            i++;
+        }
+
+      
+        while(i>9){   // false condition but no syntax error
+            System.out.println(i);
+        }
+
+        // While loop always accepts true , if you initially give false(Boolean value) 
+        // it will give syntax error.
+
+        while(false){
+            System.out.println("hello LOLU"); 
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number : ");
+        int n = sc.nextInt();
+         int j = 1;
+        while(j<=10){
+            System.out.println(j+" X "+n+" = "+n*j);
+            j++;
+        }
+
+        int j = 1;
+        int  fact = 1;
+        while(j<=n){
+            fact *= j;
+            j++;
+        }
+        System.out.println(fact);
+
+    }
+}
