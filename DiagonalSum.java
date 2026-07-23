07f20363dde93255fac0619c934be35387b47209
Nainsi yadav <yadavnainsi734@gmail.com>
2026-07-23 19:00:20 +0530

    changed file name

--- DiagonalSum.java
+++ DiagonalSum.java
@@ -1,5 +1,5 @@
 public class DiagonalSum {
-    public static int diagonalSum(int mat[][]){
+    public static void diagonalSum(int mat[][]){
         int sum = 0;
         // primary diagonalSum
         for (int i = 0; i < mat.length; i++) {
@@ -11,10 +11,25 @@ public class DiagonalSum {
                 }
             }
         }
-        return sum;
+        System.out.println("sum is : "+sum);
+    }
+    
+    // ----------optimized way --------
+    
+    public static void Dsum(int mat[][]){
+        int sum = 0;
+        for (int i = 0; i < mat.length; i++) {
+            //primary sum (i=j)
+            sum+= mat[i][i];
+            //secondry sum i+j=lenght-1 => j=lenght-1-i
+            if(i != mat.length-1-i)
+                sum+= mat[i][mat.length-1-i];
+        }
+        System.out.println("sum is : "+sum);
     }
     public static void main(String[] args) {
         int mat[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-       diagonalSum(mat);
+       //diagonalSum(mat);
+       Dsum(mat);
     }
 }
